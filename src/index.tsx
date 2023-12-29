930860dad06d53a80999336b2048e12424fb1865
Wojtek Witkowski <hi@wojtek.im>
2023-12-29 01:06:09 +0100

    error handling

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,3 @@
-// "use client";
-
 import React from "react";
 import Linkify from "linkify-react";
 
@@ -9,17 +7,21 @@ const linkifyOptions = {
 };
 
 const getCast = async (username: string, hash: string) => {
-  const res = await fetch(
-    `https://client.warpcast.com/v2/user-thread-casts?castHashPrefix=${hash}&username=${username}&limit=5`,
-  );
-  const cast = await res.json();
+  try {
+    const res = await fetch(
+      `https://client.warpcast.com/v2/user-thread-casts?castHashPrefix=${hash}&username=${username}&limit=5`,
+    );
+    const cast = await res.json();
 
-  // Handle skipping root-embed casts which are empty parents for a cast in a channel.
-  if (cast.result.casts[0].castType === "root-embed") {
-    return cast.result.casts[1];
+    // Handle skipping root-embed casts which are empty parents for a cast in a channel.
+    if (cast.result.casts[0].castType === "root-embed") {
+      return cast.result.casts[1];
+    }
+
+    return cast.result.casts[0];
+  } catch (e) {
+    throw new Error("Unable to fetch cast.");
   }
-
-  return cast.result.casts[0];
 };
 
 /**
