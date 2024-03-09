3d79e688197432967762d3efcc870340aa896572
Wojtek Witkowski <hi@wojtek.im>
2024-03-09 14:58:47 +0100

    fix the cast rendering for real this time

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,14 @@
 export const getCast = async (username: string, hash: string) => {
   try {
     const res = await fetch(
-      `https://client.warpcast.com/v2/user-thread-casts?castHashPrefix=${hash}&username=${username}&limit=5`,
+      `https://client.warpcast.com/v2/user-thread-casts?castHashPrefix=${hash}&username=${username}&limit=3`,
     );
     const cast = await res.json();
 
     // Handle weird reply to a cast in a channel returning the original cast.
-    if (cast.result.casts[2] && cast.result.casts[2].text) {
+    // We need to check if the username is matching the author of the cast to render the right one.
+    // Very weird API, but it's the only way to get the right cast.
+    if (cast.result.casts[2] && cast.result.casts[2].text && cast.result.casts[2].author.username === username) {
       return cast.result.casts[2];
     }
 
