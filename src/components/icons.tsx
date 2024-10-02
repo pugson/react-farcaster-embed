ee071ba521c321a6830b19dc63ddd6fb6924b0bc
Jonathan Zinger <thisiszinger@gmail.com>
2024-10-02 13:32:05 -0400

    don't display watches (bookmarks)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -48,22 +48,6 @@ export const LikeIcon = () => (
   </svg>
 );
 
-export const BookmarkIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
-  </svg>
-);
-
 export const WarpcastIcon = () => (
   <svg width="24" height="24" viewBox="0 0 1260 1260" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g clipPath="url(#fc-embed-clip1)">
