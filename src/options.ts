68a28a15a5414fd28d952e8ab492964dbbb4a8ae
Spencer <spencerobsitnik@gmail.com>
2025-09-21 22:19:42 -0400

    feat: add support for optional logo link (#21)
    * support for optional logo link
    
    * rename and reverse var

--- src/options.ts
+++ src/options.ts
@@ -3,6 +3,7 @@ export type FarcasterEmbedOptions = {
   timestampLocale?: string;
   customEndpoint?: string;
   silentError?: boolean;
+  hideFarcasterLogo?: boolean;
 };
 
 export const defaultOptions: FarcasterEmbedOptions = {
@@ -13,4 +14,5 @@ export const defaultOptions: FarcasterEmbedOptions = {
   },
   timestampLocale: "en-US",
   silentError: false,
+  hideFarcasterLogo: false,
 };
